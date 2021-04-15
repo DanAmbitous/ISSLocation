fcf9dbd6ab92f4257a45739d3e1e99dbe2b33979
Dan <danazad@gmail.com>
2021-04-15 14:31:23 -0500

    Speed units

--- issLocation.js
+++ issLocation.js
@@ -1,42 +1,72 @@
-const unitOptions = document.getElementById('inputs');
+const distanceUnitOptions = document.getElementById('inputs');
+const speedDistance = document.getElementById('speed-inputs');
 
-let theUnit;
-
-const unitType = () => {
-  if (unitOptions.selectedIndex === 0) {
-    theUnit = 'km';
-  } else if (unitOptions.selectedIndex === 1) {
-    theUnit = 'm';
-  } else if (unitOptions.selectedIndex === 2) {
-    theUnit = 'mi';
-  } else if (unitOptions.selectedIndex === 3) {
-    theUnit = 'ft';
-  } else if (unitOptions.selectedIndex === 4) {
-    theUnit = 'in';
-  }
-
-  console.log(theUnit, unitOptions.selectedIndex);
-}
-
-unitOptions.addEventListener('input', unitType, true);
+let distanceUnit;
+let velocityUnit;
 
 const toRound = (value) => {
   return value.toFixed(2);
 }
 
-const unitConverter = (value) => {
-  if (unitOptions.selectedIndex === 0) {
-    return value;
-  } else if (unitOptions.selectedIndex === 1) {
-    return value * 1000;
-  } else if (unitOptions.selectedIndex === 2) {
-    return value / 1.609;
-  } else if (unitOptions.selectedIndex === 3) {
-    return value * 3281;
-  } else if (unitOptions.selectedIndex === 4) {
-    return value * 39370;
+const unitConverterDistance = (value) => {
+  if (distanceUnitOptions.selectedIndex === 0) {
+    distanceUnit = 'km';
+  } else if (distanceUnitOptions.selectedIndex === 1) {
+    distanceUnit = 'm';
+  } else if (distanceUnitOptions.selectedIndex === 2) {
+    distanceUnit = 'mi';
+  } else if (distanceUnitOptions.selectedIndex === 3) {
+    distanceUnit = 'ft';
+  } else if (distanceUnitOptions.selectedIndex === 4) {
+    distanceUnit = 'in';
   }
+
+  if (distanceUnitOptions.selectedIndex === 0) {
+    return value;
+  } else if (distanceUnitOptions.selectedIndex === 1) {
+    return value * 1000;
+  } else if (distanceUnitOptions.selectedIndex === 2) {
+    return value / 1.609;
+  } else if (distanceUnitOptions.selectedIndex === 3) {
+    return value * 3281;
+  } else if (distanceUnitOptions.selectedIndex === 4) {
+    return value * 39370
+  }
+
+  toRound(value);
 }
+
+const unitConverterVelocity = (value) => {  
+  if (speedDistance.selectedIndex === 0) {
+    velocityUnit = 'kph';
+  } else if (speedDistance.selectedIndex === 1) {
+    velocityUnit = 'm/h';
+  } else if (speedDistance.selectedIndex === 2) {
+    velocityUnit = 'mph';
+  } else if (speedDistance.selectedIndex === 3) {
+    velocityUnit = 'ft/h';
+  } else if (speedDistance.selectedIndex === 4) {
+    velocityUnit = 'in/h';
+  }
+
+  if (speedDistance.selectedIndex === 0) {
+    return value;
+  } else if (speedDistance.selectedIndex === 1) {
+    return value / 1000;
+  } else if (speedDistance.selectedIndex === 2) {
+    return value / 1.609;
+  } else if (speedDistance.selectedIndex === 3) {
+    return value * 3281;
+  } else if (speedDistance.selectedIndex === 4) {
+    return value * 39370
+  }
+  
+  toRound(value);
+}
+
+distanceUnitOptions.addEventListener('input', unitConverterDistance, true);
+
+speedDistance.addEventListener('input', unitConverterVelocity, true);
 
 const worldMap = L.map('map').setView([0, 0], 1);
 
@@ -58,32 +88,33 @@ title.addTo(worldMap);
 
 const apiUrl = `https://api.wheretheiss.at/v1/satellites/25544`;
 
-let issData = async (apiUrl, distanceSymbol) => {
+let issData = async (apiUrl, distanceSymbol, speedUnit) => {
   const responseFlow = await fetch(apiUrl);
   const dataObject = await responseFlow.json();
 
-  let { latitude, longitude, velocity , altitude, units, visibility } = dataObject;
-
-  altitude = unitConverter(altitude);
+  let { latitude, longitude, velocity , altitude, visibility } = dataObject;
 
   latitude = toRound(latitude);
   longitude = toRound(longitude);
   velocity = toRound(velocity);
   altitude = toRound(altitude);
 
+  altitude = unitConverterDistance(altitude);
+  velocity = unitConverterVelocity(velocity);
+
   issLocation.setLatLng([latitude, longitude]);
 
   document.getElementById('latitude').textContent = latitude;
   document.getElementById('longitude').textContent = longitude;
-  document.getElementById('velocity').textContent = velocity;
+  document.getElementById('velocity').textContent = `${velocity} ${speedUnit}`;
   document.getElementById('altitude').textContent = `${altitude} ${distanceSymbol}`;
-  document.getElementById('units').textContent = units;
   document.getElementById('visibility').textContent = visibility;
 }
 
-unitType();
-issData(apiUrl, theUnit);
+unitConverterDistance();
+unitConverterVelocity();
+issData(apiUrl, distanceUnit, velocityUnit);
 
 setInterval(() => {
-  issData(apiUrl, theUnit);
+  issData(apiUrl, distanceUnit, velocityUnit);
 }, 1000)
